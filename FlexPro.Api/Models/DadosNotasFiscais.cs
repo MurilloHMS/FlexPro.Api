09db02a880cb7338c3e130f69eb6613e4b73473e
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-03-11 16:19:25 -0300

    finalizado rota para coletar dados das notas fiscais

--- FlexPro.Api/Models/DadosNotasFiscais.cs
+++ FlexPro.Api/Models/DadosNotasFiscais.cs
@@ -9,8 +9,10 @@ namespace FlexPro.Api.Models
     {
         public string Fornecedor { get; set; }
         public string NumeroNota { get; set; }
+        public DateTime DataNota { get; set; }
         public string Produto { get; set; }
         public decimal ValorUnitario { get; set; }
+        public decimal ValorTotal { get; set; } 
         public string CFOP { get; set; }
     }
 }
