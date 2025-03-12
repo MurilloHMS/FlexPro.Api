a517e8fbde0df26a3ed420f287790997dd6e017d
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-03-12 15:32:44 -0300

    feat: rota para cadastrar produtos

---
+++ FlexPro.Api/Controllers/ProdutoController.cs
@@ -0,0 +1,83 @@
+﻿using FlexPro.Api.Data;
+using FlexPro.Api.Interfaces;
+using FlexPro.Api.Models;
+using FlexPro.Api.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlexPro.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IProdutoRepository _produtoRepository;
+
+        public ProdutoController(AppDbContext context)
+        {
+            _context = context;
+            _produtoRepository = new ProdutoRepository(_context);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Produto>>> GetProduto()
+        {
+            var product = await _produtoRepository.GetAll();
+            return product == null ? NotFound() : Ok(product);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Produto>> GetProduto(int id)
+        {
+            var product = await _produtoRepository.GetById(id);
+            return product == null ? NotFound() : Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostProduct(Produto product)
+        {
+            await _produtoRepository.SaveOrUpdate(product);
+            return CreatedAtAction(nameof(GetProduto), new {id = product.Id}, product);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutProduct(int id,  Produto product)
+        {
+            if(id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                await _produtoRepository.SaveOrUpdate(product);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (await _produtoRepository.GetById(id) == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var product = await _produtoRepository.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            await _produtoRepository.Delete(product);
+            return NoContent();
+        }
+    }
+}
---
+++ FlexPro.Api/Repository/ProdutoRepository.cs
@@ -0,0 +1,48 @@
+﻿using FlexPro.Api.Data;
+using FlexPro.Api.Interfaces;
+using FlexPro.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlexPro.Api.Repository
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Produto>> GetAll()
+        {
+            var products = await _context.Produto.ToListAsync();
+            return products ?? Enumerable.Empty<Produto>();
+        }
+
+        public async Task<Produto> GetById(int id)
+        {
+            var product = await _context.Produto.FirstOrDefaultAsync(x => x.Id == id);
+            return product ?? null;
+        }
+
+        public async Task SaveOrUpdate(Produto produto)
+        {
+            var productFounded = await _context.Produto.FirstOrDefaultAsync(x => x.Id == produto.Id);
+            if (productFounded != null)
+            {
+                _context.Entry(productFounded).CurrentValues.SetValues(produto);
+            }
+            else
+            {
+                _context.Produto.Add(produto);
+            }
+            await _context.SaveChangesAsync();
+        }
+        public async Task Delete(Produto produto)
+        {
+            _context.Produto.Remove(produto);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
