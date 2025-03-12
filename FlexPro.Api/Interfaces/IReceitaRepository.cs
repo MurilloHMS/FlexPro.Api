b944d023978119775aef3fcab269b41f131f5cd0
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-03-12 15:32:59 -0300

    feat: rota para criar receitas

---
+++ FlexPro.Api/Interfaces/IReceitaRepository.cs
@@ -0,0 +1,12 @@
+﻿using FlexPro.Api.Models;
+
+namespace FlexPro.Api.Interfaces
+{
+    public interface IReceitaRepository
+    {
+        Task<IEnumerable<Receita>> GetAll();
+        Task<Receita> GetById(int id);
+        Task SaveOrUpdate(Receita receita);
+        Task Delete(Receita receita);
+    }
+}
