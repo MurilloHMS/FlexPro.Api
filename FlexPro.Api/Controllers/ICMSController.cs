92068f28b31ffe07b3b285d80ba3857a618586d1
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-03-17 12:31:31 -0300

    refact: melhorado responsabilidades

--- FlexPro.Api/Controllers/ICMSController.cs
+++ FlexPro.Api/Controllers/ICMSController.cs
@@ -1,9 +1,7 @@
-using System.Globalization;
-using System.Xml.Linq;
 using ClosedXML.Excel;
 using FlexPro.Api.Models;
 using Microsoft.AspNetCore.Mvc;
-using LoadOptions = System.Xml.Linq.LoadOptions;
+using FlexPro.Api.Services;
 
 namespace FlexPro.Api.Controllers;
 
@@ -30,7 +28,7 @@ public class ICMSController : ControllerBase
 
             using (var stream = file.OpenReadStream())
             {
-                var dados = await ProcessarXML(stream);
+                var dados = await IcmsService.ProcessarXML(stream);
                 if (dados != null)
                 {
                     dadosICMS.Add(dados);
@@ -72,39 +70,4 @@ public class ICMSController : ControllerBase
         return File(memoryStream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", $"ICMS-{DateTime.Now:dd-MM-yyyy}.xlsx");
     }
 
-    private async Task<ICMS> ProcessarXML(Stream stream)
-    {
-        try
-        {
-            var xmlDoc = await XDocument.LoadAsync(stream, LoadOptions.None, CancellationToken.None);
-            XNamespace ns = xmlDoc.Root.GetDefaultNamespace();
-
-            var dados = new ICMS();
-            var numNfTag = xmlDoc.Descendants(ns + "ide").FirstOrDefault();
-            if (numNfTag != null)
-            {
-                dados.nNF = numNfTag.Element(ns + "nNF")?.Value;
-            }
-
-            var icmsTag = xmlDoc.Descendants(ns + "ICMSTot").FirstOrDefault();
-            if (icmsTag != null)
-            {
-                string icms = icmsTag.Element(ns + "vICMS")?.Value;
-                dados.vICMS = !string.IsNullOrEmpty(icms) && decimal.TryParse(icms, NumberStyles.Any, CultureInfo.InvariantCulture, out decimal result) ? result : 0m;
-
-                string pis = icmsTag.Element(ns + "vPIS").Value;
-                dados.vPis = !string.IsNullOrEmpty(pis) && decimal.TryParse(pis, NumberStyles.Any, CultureInfo.InvariantCulture, out decimal pisResult) ? pisResult : 0m;
-
-                string cofins = icmsTag.Element(ns + "vCOFINS").Value;
-                dados.vCofins = !string.IsNullOrEmpty(cofins) && decimal.TryParse(cofins, NumberStyles.Any, CultureInfo.InvariantCulture, out decimal cofinsResult) ? cofinsResult : 0m;
-            }
-
-            return dados;
-        }
-        catch (Exception e)
-        {
-            return null;
-        }
-    }
-    
 }
