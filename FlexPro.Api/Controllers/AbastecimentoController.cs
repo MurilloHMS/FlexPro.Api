72f425ca3f3b78bd03e6a180bbd2d2753f0f97b7
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-03-17 16:56:05 -0300

    feat: criado endpoint Abastecimentos

---
+++ FlexPro.Api/Controllers/AbastecimentoController.cs
@@ -0,0 +1,57 @@
+﻿using FlexPro.Api.Data;
+using FlexPro.Api.Models;
+using FlexPro.Api.Repository;
+using FlexPro.Api.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlexPro.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AbastecimentoController : ControllerBase
+    {
+        private readonly AbastecimentoService _service;
+        private readonly AppDbContext _context;
+        private readonly AbastecimentoRepository _repository;
+        public AbastecimentoController(AppDbContext context)
+        {
+            _context = context;
+            _service = new AbastecimentoService(context);
+            _repository = new AbastecimentoRepository(context);
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file == null || file.Length == 0) return BadRequest();
+
+            List<Abastecimento> dadosAbastecimento = await _service.ColetarDadosAbastecimento(file);
+
+            if(dadosAbastecimento != null && dadosAbastecimento.Any())
+            {
+                foreach(var abastecimento in dadosAbastecimento)
+                {
+                    var departamento = _context.Funcionarios.FirstOrDefault(f => f.Nome.ToUpper().Contains(abastecimento.NomeDoMotorista.ToUpper()));
+                    abastecimento.Departamento = departamento != null ? departamento.Departamento : "Sem Departamento";
+                }
+                try
+                {
+                    await _repository.AddRangeFuelSupply(dadosAbastecimento);
+                }catch(Exception e)
+                {
+                    return BadRequest($"Ocorreu um erro ao salvar os dados de abastecimento {e.Message}");
+                }
+            }
+
+            return Ok(dadosAbastecimento);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAbastecimento()
+        {
+            var abastecimento = await _repository.GetFuelSupply();
+            return abastecimento != null ? Ok(abastecimento) : NotFound();
+        }
+    }
+}
--- FlexPro.Api/Interfaces/IAbastecimentoRepository.cs
+++ FlexPro.Api/Interfaces/IAbastecimentoRepository.cs
@@ -4,16 +4,12 @@ namespace FlexPro.Api.Interfaces
 {
     public interface IAbastecimentoRepository
     {
-        Task<string> CalculateGeneralFuelSupply(DateTime date);
-        Task<string> CalculateSetorFuelSupply(DateTime date);
-        Task<string> CalculateIndividualFuelSupply(DateTime date);
-        Task<string> CalculateFuelSupply(IList<Abastecimento> fuelSupplyCurrentMonth, IList<Abastecimento> fuelSupplyLastMonth, string type);
         Task AddFuelSupply(Abastecimento fuelSupply);
         Task RemoveFuelSupply(int fuelSupplyId);
-        Task AddRangeFuelSupply(IList<Abastecimento> fuelSupplies);
-        Task ExportData(IList<Abastecimento> fuelSupplies);
+        Task AddRangeFuelSupply(List<Abastecimento> fuelSupplies);
+        Task ExportData(List<Abastecimento> fuelSupplies);
         Task ImportData();
-        Task<IEnumerable<Abastecimento>> GetFuelSupply();
-        Task<IEnumerable<Abastecimento>> CollectFuelSupplyData(IFormFile file);
+        Task<List<Abastecimento>> GetFuelSupply();
+        Task<List<Abastecimento>> GetFuelSupplyByDate(DateTime initial, DateTime finish);
     }
 }
