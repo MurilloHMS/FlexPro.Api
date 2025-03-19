afd442351f65a98096f12c4b15ea936554a7daa7
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-03-19 13:48:33 -0300

    adicionado migrações do blazor server
    migrei as migrations para a api porque o blazor server que criou o banco de dados

---
+++ FlexPro.Api/Data/Migrations/20241118111337_EntidadesReceitasProdutosRevisao.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FlexPro.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class EntidadesReceitasProdutosRevisao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Entidade",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entidade", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Receita",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: false),
+                    Embalagem = table.Column<double>(type: "double precision", nullable: false),
+                    ValorMaoDeObra = table.Column<decimal>(type: "numeric", nullable: false),
+                    Caixas = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Receita", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Revisao",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Data = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Kilometragem = table.Column<int>(type: "integer", nullable: false),
+                    NotaFiscal = table.Column<string>(type: "text", nullable: false),
+                    Tipo = table.Column<string>(type: "text", nullable: false),
+                    Motorista = table.Column<string>(type: "text", nullable: false),
+                    LocalId = table.Column<int>(type: "integer", nullable: false),
+                    VeiculoId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Revisao", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Revisao_Entidade_LocalId",
+                        column: x => x.LocalId,
+                        principalTable: "Entidade",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Revisao_Veiculo_VeiculoId",
+                        column: x => x.VeiculoId,
+                        principalTable: "Veiculo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CodigoSistema = table.Column<string>(type: "text", nullable: false),
+                    Descricao = table.Column<string>(type: "text", nullable: false),
+                    QuantidadeFormula = table.Column<decimal>(type: "numeric", nullable: false),
+                    QuantidadeProducao = table.Column<decimal>(type: "numeric", nullable: false),
+                    MateriaPrima = table.Column<string>(type: "text", nullable: false),
+                    IdReceita = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Produto_Receita_IdReceita",
+                        column: x => x.IdReceita,
+                        principalTable: "Receita",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produto_IdReceita",
+                table: "Produto",
+                column: "IdReceita");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Revisao_LocalId",
+                table: "Revisao",
+                column: "LocalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Revisao_VeiculoId",
+                table: "Revisao",
+                column: "VeiculoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produto");
+
+            migrationBuilder.DropTable(
+                name: "Revisao");
+
+            migrationBuilder.DropTable(
+                name: "Receita");
+
+            migrationBuilder.DropTable(
+                name: "Entidade");
+        }
+    }
+}
---
+++ FlexPro.Api/Data/Migrations/20241119140617_AdicionandoHierarquiaNoFuncionario.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlexPro.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionandoHierarquiaNoFuncionario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Hierarquia",
+                table: "Funcionarios",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hierarquia",
+                table: "Funcionarios");
+        }
+    }
+}
