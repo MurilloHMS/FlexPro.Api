eec20d445549990447bff0f466578fde822b083e
MurilloHMS <murillohenriquemelo@outlook.com>
2025-04-14 16:28:01 -0300

    feat: incluido testes

---
+++ FlexPro.Test/Commands/CriarProdutoCommandHandlerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlexPro.Api.Application.Commands;
+using FlexPro.Test.Setup;
+using Xunit;
+using FluentAssertions;
+
+namespace FlexPro.Test.Commands
+{
+    public class CriarProdutoCommandHandlerTests
+    {
+        [Fact]
+        public async Task Handle_DeveCriarVeiculoERetornarID()
+        {
+            var context = TestDbContextFactory.CreateInMemoryContext();
+            var handler = new CriarVeiculoCommandHanddler(context);
+            var command = new CriarVeiculoCommand
+            {
+                Nome = "teste",
+                Marca = "FIAT",
+                Placa = "FFR7092"
+            };
+
+            var resultado = await handler.Handle(command, CancellationToken.None);
+
+            resultado.Should().BeGreaterThan(0);
+            var veiculoCriado = await context.Veiculo.FindAsync(resultado);
+            veiculoCriado.Should().NotBeNull();
+            veiculoCriado!.Nome.Should().Be(command.Nome);
+        }
+    }
+}
---
+++ FlexPro.Test/Setup/TestDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlexPro.Api.Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlexPro.Test.Setup
+{
+    public class TestDbContextFactory
+    {
+        public static AppDbContext CreateInMemoryContext()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            return new AppDbContext(options);
+        }
+    }
+}
