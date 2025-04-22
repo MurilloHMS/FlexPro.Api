b0880b4e86f12d736c3e6449eeb57e448ad1140b
MurilloHMS <murillohenriquemelo@outlook.com>
2025-04-22 15:00:36 -0300

    feat: incluido questpdf e configurado para criar pdf

---
+++ FlexPro.Api/API/Controllers/ReportPreviewController.cs
@@ -0,0 +1,32 @@
+﻿using FlexPro.Api.Application.Interfaces;
+using FlexPro.Api.Infrastructure.Services;
+using FlexPro.Api.Infrastructure.Templates.Reports;
+using Microsoft.AspNetCore.Mvc;
+using QuestPDF.Companion;
+
+namespace FlexPro.Api.API.Controllers
+{
+    [ApiController]
+    [Route("preview/report")]
+    public class ReportPreviewController : ControllerBase
+    {
+        private readonly AbastecimentoService _abastecimentoService;
+        private readonly IAbastecimentoRepository _abastecimentoRepository;
+
+        public ReportPreviewController(AbastecimentoService abastecimentoService, IAbastecimentoRepository abastecimentoRepository)
+        {
+            _abastecimentoService = abastecimentoService;
+            _abastecimentoRepository = abastecimentoRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> PreviewReport()
+        {
+            var abastecimentos = await _abastecimentoRepository.GetFuelSupplyByDate(new DateTime(2025, 1, 1).ToUniversalTime(), new DateTime(2025, 1, 31).ToUniversalTime());
+            var report = new FuelSuppy_Report(_abastecimentoService, _abastecimentoRepository, abastecimentos);
+            report.MetricasGeral = "Abastecimento Geral\r\nQuantidade de litros abastecidos: aumentou em 10,63% de 6.568,647 para 7.266,577\r\nMédia de KM/L: aumentou em 0,97% de 12,232 para 12,351\r\nMédia de Preço/L: aumentou em 1,23% de R$ 5,45 para R$ 5,52\r\nValor Total Gasto: aumentou em 11,48% de R$ 36.018,95 para R$ 40.152,77\r\nDistancia percorrida: aumentou em 11,36% de 80.954 para 90.153\r\n\r\n";
+            report.ShowInCompanion(); // Isso abre o PDF no Companion
+            return Ok("Report preview generated successfully.");
+        }
+
+    }
+}
---
+++ FlexPro.Api/Infrastructure/Templates/Reports/FuelSuppy_Report.cs
@@ -0,0 +1,196 @@
+﻿using FlexPro.Api.Domain.Entities;
+using FlexPro.Api.Infrastructure.Services;
+using QuestPDF.Companion;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using QuestPDF.Previewer;
+using System.Collections.Generic;
+using System.Resources;
+using ScottPlot;
+using Colors = QuestPDF.Helpers.Colors;
+using FlexPro.Api.Application.Interfaces;
+using FlexPro.Api.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using MimeKit.Cryptography;
+
+
+namespace FlexPro.Api.Infrastructure.Templates.Reports
+{
+    public class FuelSuppy_Report : IDocument
+    {
+        private readonly AbastecimentoService _abastecimentoService;
+        private readonly IAbastecimentoRepository _abastecimentoRepository;
+        private List<Abastecimento> _abastecimentosLista;
+
+        public string MetricasGeral { get; set; }
+        
+        public DocumentMetadata GetMetadata() => DocumentMetadata.Default;
+        public DocumentSettings GetSettings() => DocumentSettings.Default;
+
+        public FuelSuppy_Report(AbastecimentoService service, IAbastecimentoRepository repository, List<Abastecimento> abastecimentos)
+        {
+            _abastecimentoService = service;
+            _abastecimentoRepository = repository;
+            _abastecimentosLista = abastecimentos;
+        }
+
+        public void Compose(IDocumentContainer container)
+        {
+            container.Page(page =>
+            {
+                page.Margin(15);
+                page.Header().Element(ComposeHeader);
+                page.Content().Element(ComposeContent);
+                page.Footer().Element(ComposeFooter);
+            });
+
+            container.Page(page =>
+            {
+                page.Margin(15);
+                page.Header().Element(ComposeHeader);
+                page.Footer().Element(ComposeFooter);
+            });
+        }
+
+        private void ComposeHeader(IContainer container)
+        {
+            var logo = Properties.Resources.Logo_Proauto;
+            container.Row(row =>
+            {
+                row.RelativeItem().Column(column =>
+                {
+                    column.Item().Text("Relatório de Abastecimento").FontSize(20).SemiBold().FontColor(Colors.Blue.Medium);
+                    column.Item().Text($"Data de Emissão: {DateTime.Now:dd/MM/yyyy}")
+                    .FontSize(10)
+                    .FontColor(Colors.Grey.Darken2);
+                });
+
+                row.ConstantItem(200).Height(50).Image(logo).FitWidth();
+            });
+        }
+
+        private void ComposeContent(IContainer container)
+        {
+            
+            container.Padding(20)
+                .Column(col =>
+                {
+                    col.Item().PaddingBottom(20).Text("Controle Geral").FontSize(24).Bold().AlignCenter();
+
+                    col.Item().AspectRatio(2)
+                        .Svg(size =>
+                        {
+                            Plot plot = new();
+
+                            var dados = _abastecimentosLista.GroupBy(a => a.Departamento).Select(g => new
+                            {
+                                Departamento = g.Key,
+                                Litros = g.Sum(a => a.Litros),
+                                KmPercorridos = g.Sum(a => a.DiferencaHodometro),
+                                TotalGasto = g.Sum(a => (double)a.ValorTotalTransacao),
+                                MediaKmL = g.Average(a => a.MediaKm)
+                            }).ToList();
+
+                            int departamentosCount = dados.Count;
+                            double barSpacing = 1.0;
+                            double barWidth = 0.3;
+
+                            double[] xBase = Enumerable.Range(0, departamentosCount).Select(i => (double)i).ToArray();
+
+                            var barsLitros = plot.Add.Bars(xBase.Select(x => x - barWidth).ToArray(), dados.Select(d => d.Litros).ToArray());
+                            barsLitros.Color = ScottPlot.Colors.Blue;
+                            barsLitros.LegendText = "Litros";
+
+                            var barsKm = plot.Add.Bars(xBase, dados.Select(d => d.KmPercorridos).ToArray());
+                            barsKm.Color = ScottPlot.Colors.Green;
+                            barsKm.LegendText = "KM";
+
+                            var barsGasto = plot.Add.Bars(xBase.Select(x => x + barWidth).ToArray(), dados.Select(d => d.TotalGasto).ToArray());
+                            barsGasto.Color = ScottPlot.Colors.Orange;
+                            barsGasto.LegendText = "R$ Gasto";
+
+                            
+
+                            var ticks = xBase.Select((x, i) => new Tick(x, dados[i].Departamento)).ToArray();
+                            plot.Axes.Bottom.TickGenerator = new ScottPlot.TickGenerators.NumericManual(ticks);
+
+                            // Estilo
+                            plot.Legend.IsVisible = true;
+                            plot.Legend.Alignment = Alignment.UpperRight;
+                            plot.Axes.Bottom.TickLabelStyle.FontSize = 10;
+                            plot.Grid.XAxisStyle.IsVisible = false;
+                            plot.Axes.Margins(bottom: 0.2f, top: 0.2f);
+
+                            return plot.GetSvgXml((int)size.Width, (int)size.Height);
+                        });
+
+                    col.Item().Table(table =>
+                    {
+                        table.ColumnsDefinition(col =>
+                        {
+                            col.ConstantColumn(120);
+                            col.ConstantColumn(100);
+                            col.ConstantColumn(100);
+                            col.ConstantColumn(100);
+                            col.ConstantColumn(100);
+                        });
+
+                        table.Header(h =>
+                        {
+                            h.Cell().BorderBottom(2).Padding(8).Text("Departamentos").AlignCenter();
+                            h.Cell().BorderBottom(2).Padding(8).Text("Litros").AlignCenter();
+                            h.Cell().BorderBottom(2).Padding(8).Text("Km Percorrido").AlignCenter();
+                            h.Cell().BorderBottom(2).Padding(8).Text("Total Gasto").AlignCenter();
+                            h.Cell().BorderBottom(2).Padding(8).Text("Média KM").AlignCenter();
+                        });
+
+                        var dados = _abastecimentosLista.GroupBy(a => a.Departamento).Select(g => new
+                        {
+                            Departamento = g.Key,
+                            Litros = g.Sum(a => a.Litros),
+                            KmPercorridos = g.Sum(a => a.DiferencaHodometro),
+                            TotalGasto = g.Sum(a => (double)a.ValorTotalTransacao),
+                            MediaKmL = g.Average(a => a.MediaKm)
+                        }).ToList();
+
+                        foreach (var i in dados)
+                        {
+                            table.Cell().Padding(8).Text(i.Departamento).AlignCenter().FontSize(10);
+                            table.Cell().Padding(8).Text(i.Litros.ToString("N0")).AlignCenter();
+                            table.Cell().Padding(8).AlignCenter().Text(i.KmPercorridos);
+                            table.Cell().Padding(8).Text($"R$ {i.TotalGasto.ToString("N")}").AlignCenter();
+                            table.Cell().Padding(8).Text(i.MediaKmL.ToString("N0")).AlignCenter();
+                        }
+                    });
+
+                    col.Item().Background(Colors.Grey.Lighten3).Padding(10).Column(col =>
+                    {
+                        col.Spacing(5);
+                        col.Item().Text("Metricas").FontSize(14);
+                        col.Item().Text(MetricasGeral).FontSize(11);
+                    });
+                });
+            
+        }
+
+        private void ComposeFooter(IContainer container)
+        {
+            container.Row(row =>
+            {
+                row.RelativeItem().Column(col =>
+                {
+                    col.Item().LineHorizontal(1).LineColor(Colors.Grey.Lighten2);
+                    col.Item().Text("FlexPro • Sistema de Automação").FontSize(9).FontColor(Colors.Grey.Darken1);
+                });
+
+                row.ConstantItem(100).AlignRight().Text(x =>
+                {
+                    x.CurrentPageNumber();
+                    x.Span(" / ");
+                    x.TotalPages();
+                });
+            });
+        }
+    }
+}
