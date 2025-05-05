34c930f4d3ac9dc23dc5ddd2216f61c57a4f48cb
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-05-05 16:26:12 -0300

    feat: incluido email de teste e importar todos os clientes

--- FlexPro.Api/Controllers/ClientesController.cs
+++ FlexPro.Api/Controllers/ClientesController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ClosedXML.Excel;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using FlexPro.Api.Infrastructure.Persistance;
 using FlexPro.Api.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
 
 namespace FlexPro.Api.Controllers
 {
@@ -71,6 +73,38 @@ namespace FlexPro.Api.Controllers
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetCliente", new { id = cliente.Id }, cliente);
+        }
+        [AllowAnonymous]
+        [HttpPost("AllEmployees")]
+        public async Task<IActionResult> CreateEmployeeData(IFormFile file)
+        {
+            var clientes = new List<Cliente>();
+            using (var stream = new MemoryStream())
+            {
+                await file.CopyToAsync(stream);
+                stream.Position = 0;
+
+                using (XLWorkbook wb = new XLWorkbook(stream))
+                {
+                    var planilha = wb.Worksheets.FirstOrDefault();
+                    var dados = planilha.RowsUsed().Skip(1).Select(row => new Cliente
+                    {
+                        CodigoSistema = row.Cell(1).TryGetValue<string>(out var codigoSistemaCliente)
+                            ? codigoSistemaCliente
+                            : default,
+                        Nome = row.Cell(2).TryGetValue<string>(out var nomeParceiro) ? nomeParceiro : default,
+                        Email = row.Cell(3).TryGetValue<string>(out var email) ? email : default,
+                        Status = "true"
+                    }).ToList();
+                    
+                    clientes.AddRange(dados);
+                }
+            }
+            
+            _context.Cliente.AddRange(clientes);
+            await _context.SaveChangesAsync();
+
+            return Ok();
         }
 
 
--- FlexPro.Api/Domain/Entities/Cliente.cs
+++ FlexPro.Api/Domain/Entities/Cliente.cs
@@ -7,5 +7,6 @@
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Status { get; set; }
+        public string EmailTeste { get; set; } = "murillo.henrique@proautokimium.com.br";
     }
 }
