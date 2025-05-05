6624a06469e2f3cfa7d047dc64294ecf21290905
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-05-05 16:27:03 -0300

    incluido migration dos clientes

---
+++ FlexPro.Api/Infrastructure/Migrations/20250505152504_alteracaoClienteEmailTeste.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlexPro.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class alteracaoClienteEmailTeste : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmailTeste",
+                table: "Cliente",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailTeste",
+                table: "Cliente");
+        }
+    }
+}
