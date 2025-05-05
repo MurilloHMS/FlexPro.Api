592441f7dbe259b7675d9fdb57858f8f953e4d8d
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-05-05 10:04:10 -0300

    test: criado teste dos controllers
    - criado teste do envio de emails com status code 200 e 400
    - criado teste do envio de emails informativos com status code 200 e 400

---
+++ FlexPro.Api/API/Controllers/EmailController.cs
@@ -0,0 +1,32 @@
+using FlexPro.Api.Application.Commands.Email;
+using FlexPro.Api.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlexPro.Api.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EmailController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public EmailController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("send")]
+    public async Task<IActionResult> SendEmailAsync(Email emailData)
+    {
+        var result = await _mediator.Send(new SendEmailCommand(emailData));
+        return result;
+    }
+    
+    [HttpPost("send/informativos")]
+    public async Task<IActionResult> SendInformativosAsync(List<Informativo> informativos)
+    {
+        var result = await _mediator.Send(new SendInformativoCommand(informativos));
+        return result;
+    }
+}
