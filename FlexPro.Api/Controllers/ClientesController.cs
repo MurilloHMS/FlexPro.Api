8a9d8ea5b35b0b09e607051be349e2a1cb3b86d3
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-05-06 09:58:18 -0300

    removido funcoes do controller antigo

--- FlexPro.Api/Controllers/ClientesController.cs
+++ FlexPro.Api/Controllers/ClientesController.cs
@@ -17,64 +17,7 @@ namespace FlexPro.Api.Controllers
         {
             _context = context;
         }
-
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Cliente>>> GetCliente()
-        {
-            return await _context.Cliente.ToListAsync();
-        }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Cliente>> GetCliente(int id)
-        {
-            var cliente = await _context.Cliente.FindAsync(id);
-
-            if (cliente == null)
-            {
-                return NotFound();
-            }
-
-            return cliente;
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutCliente(int id, Cliente cliente)
-        {
-            if (id != cliente.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(cliente).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ClienteExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        [HttpPost]
-        public async Task<ActionResult<Cliente>> PostCliente(Cliente cliente)
-        {
-            _context.Cliente.Add(cliente);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetCliente", new { id = cliente.Id }, cliente);
-        }
-        [AllowAnonymous]
+        
         [HttpPost("AllEmployees")]
         public async Task<IActionResult> CreateEmployeeData(IFormFile file)
         {
@@ -105,27 +48,6 @@ namespace FlexPro.Api.Controllers
             await _context.SaveChangesAsync();
 
             return Ok();
-        }
-
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCliente(int id)
-        {
-            var cliente = await _context.Cliente.FindAsync(id);
-            if (cliente == null)
-            {
-                return NotFound();
-            }
-
-            _context.Cliente.Remove(cliente);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool ClienteExists(int id)
-        {
-            return _context.Cliente.Any(e => e.Id == id);
         }
     }
 }
