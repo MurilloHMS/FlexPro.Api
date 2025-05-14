cdad47af22b475c9ae93dc34b6f409f956c7f338
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-05-14 12:25:36 -0300

    feat: criado enumeradores

---
+++ FlexPro.Api/Domain/Enums/StatusPedido_e.cs
@@ -0,0 +1,8 @@
+namespace FlexPro.Api.Domain;
+
+public enum StatusPedido_e
+{
+    Solicitado = 1,
+    EmAndamento = 3,
+    Finalizado = 4
+}
---
+++ FlexPro.Api/Domain/Enums/TipoContato_e.cs
@@ -0,0 +1,14 @@
+namespace FlexPro.Api.Domain;
+
+public enum TipoContato_e
+{
+    DuvidaProduto = 1,
+    SuporteTecnico = 2,
+    SolicitacaoOrcamento = 3,
+    RepresentacaoComercial = 4,
+    TrabalheConosco = 5,
+    ConsultoriaEspecializada = 6,
+    VisitaTecnica = 7,
+    InformacoesEmpresa = 8,
+    Outros = 9,
+}
