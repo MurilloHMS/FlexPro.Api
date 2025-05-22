5e3f0911fefad951826bf3f0c68b13e8754f174a
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-05-22 12:49:40 -0300

    feat: incluído repository generico

---
+++ FlexPro.Api/Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,22 @@
+using FlexPro.Api.Application.Interfaces;
+using FlexPro.Api.Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlexPro.Api.Infrastructure.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    protected readonly AppDbContext _context;
+    protected readonly DbSet<T> _dbSet;
+
+    public Repository(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+    
+    public async Task<IEnumerable<T>> GetAllAsync() => await _dbSet.ToListAsync();
+
+    public async Task<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
+    public void Delete(T entity) => _dbSet.Remove(entity);
+}
