79d9e2e61c3ff25fb0dced10231d17a311b63937
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-07-07 11:11:19 -0300

    removed allowAnonymous on ComputerController.cs

--- FlexPro.Api/API/Controllers/ComputerController.cs
+++ FlexPro.Api/API/Controllers/ComputerController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FlexPro.Api.API.Controllers;
 
-[AllowAnonymous]
 [ApiController]
 [Route("api/[controller]")]
 public class ComputerController : ControllerBase
