2680f3bafec9aea1ff25d1883c3f37420258c1f2
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-07-29 15:07:02 -0300

    refact: include new primary constructor

---
+++ FlexPro.Infrastructure/Repositories/ContatoRepository.cs
@@ -0,0 +1,37 @@
+using FlexPro.Domain.Entities;
+using FlexPro.Domain.Enums;
+using FlexPro.Domain.Repositories;
+using FlexPro.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlexPro.Infrastructure.Repositories;
+
+public class ContatoRepository(AppDbContext context) : Repository<Contato>(context), IContatoRepository
+{
+    private readonly DbSet<Contato> _dbSet = context.Set<Contato>();
+    public async Task InsertOrUpdateContatoAsync(Contato contato)
+    {
+        var ContatoFounded = await _dbSet.FirstOrDefaultAsync(x => x.Id == contato.Id);
+        if (ContatoFounded != null)
+        {
+            context.Entry(contato).CurrentValues.SetValues(contato);
+        }
+        else
+        {
+            _dbSet.Add(contato);
+        }
+        await context.SaveChangesAsync();
+    }
+
+    public async Task<List<Contato>> GetContatosByStatusAsync(StatusContato_e status)
+    {
+        var contatos = await context.Contato.Where(x => x.StatusContato == status).ToListAsync();
+        return contatos;
+    }
+
+    public async Task<List<Contato>> GetContatosByTipoAsync(TipoContato_e tipo)
+    {
+        var contatos = await context.Contato.Where(x => x.TipoContato == tipo).ToListAsync();
+        return contatos;
+    }
+}
