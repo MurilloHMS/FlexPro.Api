6a92769e8a0bff78d34d32e024629b7d09f835f3
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-07-31 10:07:48 -0300

    feat(categoryMap): include mappings and remove data annotations

--- FlexPro.Domain/Entities/Categoria.cs
+++ FlexPro.Domain/Entities/Categoria.cs
@@ -1,11 +1,9 @@
-using System.ComponentModel.DataAnnotations;
 using FlexPro.Domain.Abstractions;
 
 namespace FlexPro.Domain.Entities
 {
     public class Categoria : Entity
     {
-        [Required]
         public string Nome { get; set; } = string.Empty;
     }
 }
--- FlexPro.Infrastructure/Data/Mappings/CategoryMap.cs
+++ FlexPro.Infrastructure/Data/Mappings/CategoryMap.cs
@@ -1,6 +1,19 @@
+using FlexPro.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace FlexPro.Infrastructure.Data.Mappings;
 
-public class CategoryMap
+public class CategoryMap : EntityBaseMap<Categoria>
 {
-    
+    public override void Configure(EntityTypeBuilder<Categoria> builder)
+    {
+        base.Configure(builder);
+        
+        builder.Property(x => x.Nome)
+            .HasColumnName("name")
+            .HasColumnType("varchar")
+            .IsRequired()
+            .HasMaxLength(50);
+    }
 }
