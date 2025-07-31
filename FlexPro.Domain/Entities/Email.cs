ade1cf28fd2f73f697784f281ad946053a73a30c
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-07-31 08:58:51 -0300

    feat: create new map and remove data annotations

--- FlexPro.Domain/Entities/Email.cs
+++ FlexPro.Domain/Entities/Email.cs
@@ -3,14 +3,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 using FlexPro.Domain.Abstractions;
 
 namespace FlexPro.Domain.Entities;
-
-[Table("emails_smtp")]
 public class Email : Entity
 {
-    [Column("email_address")]
-    [MaxLength(50)]
-    public string? Address { get; set; }
-    [Column("is_enabled")]
+    public string Address { get; set; } = string.Empty;
     public bool IsEnabled { get; set; } = true;
 
 }
---
+++ FlexPro.Infrastructure/Data/Mappings/EmailMap.cs
@@ -0,0 +1,23 @@
+using FlexPro.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FlexPro.Infrastructure.Data.Mappings;
+
+public class EmailMap : EntityBaseMap<Email>
+{
+    public override void Configure(EntityTypeBuilder<Email> builder)
+    {
+        base.Configure(builder);
+
+        builder.ToTable("emails_smtp");
+
+        builder.Property(x => x.Address)
+            .HasMaxLength(50)
+            .HasColumnName("email_address")
+            .IsRequired(true);
+
+        builder.Property(x => x.IsEnabled)
+            .HasColumnName("is_enabled");
+    }
+}
