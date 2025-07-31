f18193265266792999818e29d71b583e6482ca6d
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-07-31 10:06:17 -0300

    feat(archiveMap): include mappings and remove data annotations

--- FlexPro.Domain/Entities/Arquivo.cs
+++ FlexPro.Domain/Entities/Arquivo.cs
@@ -1,26 +1,14 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using FlexPro.Domain.Abstractions;
 
 namespace FlexPro.Domain.Entities;
-
-[Table("archives")]
 public class Arquivo : Entity
 {
-    [Column("file_name")] 
-    [MaxLength(100)] 
     public string Name { get; set; } = string.Empty;
-    [Column("file_extensions")]
     public string? Extensions { get; set;}
-    [Column("file_size")]
     public long Size { get; set; }
-    [Column("created_at")]
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    [Column("updated_at")]
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
-    [Column("is_active")]
     public bool IsActive { get; set; } = true;
-    [Column("is_public")]
     public bool IsPublic { get; set; } = false;
 
 }
---
+++ FlexPro.Infrastructure/Data/Mappings/ArchiveMap.cs
@@ -0,0 +1,52 @@
+using FlexPro.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FlexPro.Infrastructure.Data.Mappings;
+
+public class ArchiveMap : EntityBaseMap<Arquivo>
+{
+    public override void Configure(EntityTypeBuilder<Arquivo> builder)
+    {
+        base.Configure(builder);
+
+        builder.ToTable("archives");
+        
+        builder.Property(x => x.Name)
+            .HasColumnName("file_name")
+            .HasMaxLength(100)
+            .HasColumnType("varchar")
+            .IsRequired();
+
+        builder.Property(x => x.Extensions)
+            .HasColumnName("file_extensions")
+            .HasMaxLength(6)
+            .HasColumnType("varchar")
+            .IsRequired(false);
+        
+        builder.Property(x => x.Size)
+            .HasColumnName("file_size")
+            .HasColumnType("bigint")
+            .IsRequired();
+
+        builder.Property(x => x.CreatedAt)
+            .HasColumnName("created_at")
+            .HasColumnType("datetime")
+            .IsRequired();
+            
+        builder.Property(x => x.UpdatedAt)
+            .HasColumnName("updated_at")
+            .HasColumnType("datetime")
+            .IsRequired();
+        
+        builder.Property(x => x.IsActive)
+            .HasColumnName("is_active")
+            .HasColumnType("boolean")
+            .IsRequired();
+        
+        builder.Property(x => x.IsPublic)
+            .HasColumnName("is_public")
+            .HasColumnType("boolean")
+            .IsRequired();
+    }
+}
