074af8473fcd18f21c48a8762ace39b9bbcee808
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-07-31 09:29:12 -0300

    feat: include InventoryMovementMap and remove data annotations

--- FlexPro.Domain/Entities/InventoryMovement.cs
+++ FlexPro.Domain/Entities/InventoryMovement.cs
@@ -1,17 +1,10 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using FlexPro.Domain.Abstractions;
 
 namespace FlexPro.Domain.Entities;
 
-[Table("inventory_movements")]
 public class InventoryMovement : Entity
 {
-    
-    [Column("system_code")]
     public string SystemId { get; set; } = String.Empty;
-    [Column("date")]
     public DateTime? Data { get; set; }
-    [Column("quantity")]
     public int Quantity { get; set; }
 }
