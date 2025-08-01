86ea1aac919f62fdba5f24cd8493799e1ff6a073
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-08-01 08:22:59 -0300

    feat(getVehicleById): create GetVehicleByID Command

--- FlexPro.Application/UseCases/Vehicles/GetById/Handler.cs
+++ FlexPro.Application/UseCases/Vehicles/GetById/Handler.cs
@@ -1,5 +1,18 @@
+using AutoMapper;
+using FlexPro.Application.DTOs;
+using FlexPro.Domain.Abstractions;
+using FlexPro.Domain.Repositories;
+using MediatR;
+
 namespace FlexPro.Application.UseCases.Vehicles.GetById;
 
-public class Handler
+public sealed class Handler(IVeiculoRepository repository, IMapper mapper) :  IRequestHandler<Command, Result<Response>>
 {
+    public async Task<Result<Response>> Handle(Command request, CancellationToken cancellationToken)
+    {
+        var vehicle = await repository.GetByIdAsync(request.Id);
+        return vehicle != null
+            ? Result.Success(new Response(mapper.Map<VeiculoDto>(vehicle)))
+            : Result.Failure<Response>(new Error("404", "Veiculo não encontrado"));
+    }
 }
