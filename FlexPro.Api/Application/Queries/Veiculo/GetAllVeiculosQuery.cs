6485b49bc4151a085da2ff7914ada36acd68052a
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-08-07 08:06:01 -0300

    chore: remove unused files

--- FlexPro.Api/Application/Queries/Veiculo/GetAllVeiculosQuery.cs
+++
@@ -1,8 +0,0 @@
-﻿using FlexPro.Application.DTOs;
-using MediatR;
-
-namespace FlexPro.Api.Application.Queries.Veiculo;
-
-public class GetAllVeiculosQuery : IRequest<IEnumerable<VeiculoDto>>
-{
-}
--- FlexPro.Api/Application/Queries/Veiculo/GetVeiculoByIdQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using FlexPro.Application.DTOs;
-using MediatR;
-
-namespace FlexPro.Api.Application.Queries.Veiculo;
-
-public class GetVeiculoByIdQuery : IRequest<VeiculoDto>
-{
-    public int Id { get; set; }
-}
