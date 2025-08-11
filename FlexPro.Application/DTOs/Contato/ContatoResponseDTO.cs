42481692b567eda6d8face877b1b9483a17c8ffa
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-08-11 08:07:55 -0300

    chore(dto): update dto's

--- FlexPro.Application/DTOs/Contato/ContatoResponseDTO.cs
+++ FlexPro.Application/DTOs/Contato/ContatoResponseDTO.cs
@@ -2,15 +2,7 @@ using FlexPro.Domain.Enums;
 
 namespace FlexPro.Application.DTOs.Contato;
 
-public class ContatoResponseDto
+public class ContatoResponseDto :  ContatoRequestDto
 {
     public int Id { get; set; }
-    public string Nome { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public TipoContatoE TipoContato { get; set; }
-    public string? Outro { get; set; }
-    public string Mensagem { get; set; } = string.Empty;
-    public string NomeEmpresa { get; set; } = string.Empty;
-    public StatusContatoE StatusContato { get; set; }
-    public DateTime DataSolicitadoContato { get; set; }
 }
--- FlexPro.Application/DTOs/ProdutoLoja/ProdutoLojaResponseDTO.cs
+++ FlexPro.Application/DTOs/ProdutoLoja/ProdutoLojaResponseDTO.cs
@@ -1,14 +1,6 @@
 namespace FlexPro.Application.DTOs.ProdutoLoja;
 
-public class ProdutoLojaResponseDto
+public class ProdutoLojaResponseDto : ProdutoLojaRequestDto
 {
     public int Id { get; set; }
-    public string CodigoSistema { get; set; } = string.Empty;
-    public string Nome { get; set; } = string.Empty;
-    public string Descricao { get; set; } = string.Empty;
-    public string Cor { get; set; } = string.Empty;
-    public string Diluicao { get; set; } = string.Empty;
-    public byte[]? Imagem { get; set; }
-    public List<EmbalagemResponseDto>? Embalagems { get; set; }
-    public List<DepartamentoResponseDto>? Departamentos { get; set; }
 }
