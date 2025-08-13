0c1ee5ad5d28f7f4e1284cb4f0885e90466eaf0b
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-08-13 11:40:08 -0300

    feat(migration): include migration

--- FlexPro.Infrastructure/Migrations/20250813143840_IncludeInventoryMovementAndProducts.Designer.cs
+++ FlexPro.Infrastructure/Migrations/20250813143840_IncludeInventoryMovementAndProducts.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FlexPro.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FlexPro.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250813143840_IncludeInventoryMovementAndProducts")]
+    partial class IncludeInventoryMovementAndProducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
